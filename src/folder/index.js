c5d09567added83f0f1a9cef69c538f344719cee
Gabriel Oliveira Santos <gabryelzinho321@gmail.com>
2023-04-17 19:26:05 -0300

    AC02

---
+++ src/folder/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>AC02</h1>
+      <p>AC02 - Frmeworkds FullStack</p>
+    </div>
+  );
+};
+
+export default About;
